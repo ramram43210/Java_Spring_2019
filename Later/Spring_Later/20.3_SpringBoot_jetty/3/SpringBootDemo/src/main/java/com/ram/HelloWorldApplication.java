45cba6225ce58a9bd763af62de4cc33600d4a958
Ram <ramram_43210@yahoo.com>
2019-06-11 15:55:01 +0530

    1

---
+++ Later/Spring_Later/20.3_SpringBoot_jetty/3/SpringBootDemo/src/main/java/com/ram/HelloWorldApplication.java
@@ -0,0 +1,36 @@
+package com.ram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class HelloWorldApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(HelloWorldApplication.class, args);
+	}
+
+	@Bean
+	public ConfigurableServletWebServerFactory webServerFactory()
+	{
+		JettyServletWebServerFactory factory = new JettyServletWebServerFactory();
+		factory.setPort(9090);
+		factory.setContextPath("/myContext");
+		return factory;
+	}
+
+	@RequestMapping(value = "/hello")
+	public String hello()
+	{
+		return "Hello World";
+	}
+
+}
