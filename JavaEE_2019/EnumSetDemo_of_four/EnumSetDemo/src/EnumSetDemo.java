0766b0c575bce32a0dd0e691265fadee5424c702
Ram <ramram_43210@yahoo.com>
2019-07-12 10:46:46 +0530

    1

---
+++ JavaEE_2019/EnumSetDemo_of_four/EnumSetDemo/src/EnumSetDemo.java
@@ -0,0 +1,21 @@
+import java.util.EnumSet;
+
+public class EnumSetDemo
+{
+
+	public enum Month
+	{
+		JAN, FEB, MAR, APR, MAY
+	};
+
+	public static void main(String[] args)
+	{
+		/*
+		 * Returns: an enum set initially containing the specified elements
+		 */
+
+		EnumSet<Month> enumSet = EnumSet.of(Month.MAR, Month.JAN, Month.MAY, Month.FEB);
+		System.out.println("enumSet = " + enumSet);
+	}
+
+}
