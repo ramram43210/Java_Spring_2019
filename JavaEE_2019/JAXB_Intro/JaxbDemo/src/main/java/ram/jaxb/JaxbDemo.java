b81d050a7b13374265b43c3ec8e4c45012a84f40
Ram <ramram_43210@yahoo.com>
2019-08-16 11:26:18 +0530

    1

---
+++ JavaEE_2019/JAXB_Intro/JaxbDemo/src/main/java/ram/jaxb/JaxbDemo.java
@@ -0,0 +1,52 @@
+package ram.jaxb;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.PropertyException;
+import javax.xml.bind.Unmarshaller;
+
+public class JaxbDemo
+{
+	public static void main(String[] args) throws Exception
+	{
+		convertObjectToXml();
+		convertXmlToObject();
+	}
+
+	private static void convertObjectToXml()
+			throws JAXBException, PropertyException, FileNotFoundException
+	{
+		JAXBContext jaxbContext = JAXBContext.newInstance(Employee.class);
+
+		Marshaller marshaller = jaxbContext.createMarshaller();
+		marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+		Employee employee = new Employee(1, "Peter", 50000);
+
+		marshaller.marshal(employee, new FileOutputStream("employee.xml"));
+		System.out.println("employee.xml is created successfully");
+	}
+
+	private static void convertXmlToObject()
+	{
+		try
+		{
+			File file = new File("employee.xml");
+			JAXBContext jaxbContext = JAXBContext.newInstance(Employee.class);
+
+			Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+			Employee employee = (Employee) unmarshaller.unmarshal(file);
+			System.out.println(employee);
+
+		}
+		catch (JAXBException e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
