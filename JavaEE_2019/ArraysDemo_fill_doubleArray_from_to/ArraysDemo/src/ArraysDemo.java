7f2a9d5f86286260841d9082df724acdc5e54167
Ram <ramram_43210@yahoo.com>
2019-10-14 09:59:45 +0530

    1

---
+++ JavaEE_2019/ArraysDemo_fill_doubleArray_from_to/ArraysDemo/src/ArraysDemo.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class ArraysDemo
+{
+	public static void main(String[] args)
+	{
+		double[] doubleArray = new double[] { 2.1, 7.2, 9.4, 1.4 };
+
+		System.out.println("Before fill = " + Arrays.toString(doubleArray));
+
+		Arrays.fill(doubleArray, 0, 3, 8.2);
+
+		System.out.println("After fill  = " + Arrays.toString(doubleArray));
+	}
+
+}
---
+++ JavaEE_2019/ArraysDemo_fill_floatArray_from_to/ArraysDemo/src/ArraysDemo.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class ArraysDemo
+{
+	public static void main(String[] args)
+	{
+		float[] floatArray = new float[] { 2.1f, 7.2f, 9.4f, 1.4f };
+
+		System.out.println("Before fill = " + Arrays.toString(floatArray));
+
+		Arrays.fill(floatArray, 0, 3, 8.2f);
+
+		System.out.println("After fill  = " + Arrays.toString(floatArray));
+	}
+
+}
