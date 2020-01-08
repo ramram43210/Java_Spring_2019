b6a06f899416d2e0bbee4ef51c0e8c97870f9321
Ram <ramram_43210@yahoo.com>
2020-01-08 10:42:37 +0530

    1

---
+++ JavaEE_2020/Spring_Boot_JNDI_Tomcat_9/SpringBootDemo/src/main/java/com/ram/Application.java
@@ -0,0 +1,22 @@
+package com.ram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
+	{
+		return application.sources(Application.class);
+	}
+
+}
