14ebd34de34bc425f27a34e274f65d06e3480d4c
Ram <ramram_43210@yahoo.com>
2020-01-17 17:09:47 +0530

    1

---
+++ JavaEE_2020/Spring_Boot_Create_Employee/SpringBootDemo/src/main/java/com/ram/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,21 @@
+package com.ram.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ram.model.Employee;
+import com.ram.repository.EmployeeRepository;
+import com.ram.service.EmployeeService;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService
+{
+
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	
+	public Employee createEmployee(Employee employee)
+	{
+		return employeeRepository.createEmployee(employee);
+	}
+}
