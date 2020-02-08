d5850244fb3c8b797904c4e9758028bb79e9daa6
Ram <ramram_43210@yahoo.com>
2020-02-08 10:13:51 +0530

    1

---
+++ JavaEE_2020/ArraysDemo_Stream_doubleArray_range/ArraysDemo/src/ArraysDemo.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.stream.DoubleStream;
+
+public class ArraysDemo
+{
+
+	public static void main(String[] args)
+	{
+		double[] doubleArray = new double[] { 5.6, 15.5, 25.1, 35.3, 45.5 };
+		DoubleStream doubleStream = Arrays.stream(doubleArray, 2, 4);
+		doubleStream.forEach(System.out::println);
+	}
+
+}
