6d90675a8d07e8da8b8681b3c3e1c47d81d965e4
Ram N <ramram_43210@yahoo.com>
2020-11-20 10:16:33 +0530

    1

---
+++ JavaEE_2020/Creating_doc_Javascript_file/studentsInfo.js
@@ -0,0 +1,37 @@
+db.studentInfo.insert(
+{
+name:{firstName:"John", lastName:"Doe"},
+class:6,
+rollNo: 23,
+subjects:["Maths","Physics","English","Chemistry"],
+attendance: {
+January: "90%",
+February:"85%",
+March:"98%"
+}
+}
+);
+db.studentInfo.insert(
+{
+name:{firstName:"Peter", lastName:"A"},
+rollNo: 24,
+subjects:["Maths","Physics","English","Chemistry"],
+attendance: {
+January: "97%%",
+February:"99%",
+March:"100%"
+}
+}
+);
+db.studentInfo.insert(
+{
+name:{firstName:"Dave", lastName:"J"},
+rollNo: 27,
+subjects:["Maths","Physics","English","Chemistry"],
+attendance: {
+January: "87%%",
+February:"99%",
+March:"100%"
+}
+}
+);
